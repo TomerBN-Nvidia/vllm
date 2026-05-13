shaharmor98/smor-vllm

test vllm serve echo trick processing

vllm/v1/core/sched/scheduler.py
@@ -348,16 +348,29 @@ class Scheduler(SchedulerInterface):
         encoder_compute_budget = self.max_num_encoder_input_tokens
         # Spec decode-related.
         scheduled_spec_decode_tokens: dict[str, list[int]] = {}
+        score_only_batch: bool | None = None
 
         # For logging.
         scheduled_timestamp = time.monotonic()
 
         self.kv_cache_manager.new_step_starts()
 
+        def matches_score_only_batch(request: Request) -> bool:
+            return score_only_batch is None or request.score_only == score_only_batch
+
+        def set_score_only_batch(request: Request) -> None:
+            nonlocal score_only_batch
+            if score_only_batch is None:
+                score_only_batch = request.score_only
+
         # First, schedule the RUNNING requests.
         req_index = 0
         while req_index < len(self.running) and token_budget > 0:
             request = self.running[req_index]
+
+            if not matches_score_only_batch(request):
+                req_index += 1
+                continue
 
             if (
                 request.num_output_placeholders > 0
@@ -461,6 +474,8 @@ class Scheduler(SchedulerInterface):
                             preempted_encoder_inputs = scheduled_encoder_inputs.pop(
                                 preempted_req_id, None
                             )
+                            if not num_scheduled_tokens:
+                                score_only_batch = None
                             if preempted_encoder_inputs:
                                 # Restore encoder compute budget if the preempted
                                 # request had encoder inputs scheduled in this step.
@@ -485,6 +500,7 @@ class Scheduler(SchedulerInterface):
 
             # Schedule the request.
             scheduled_running_reqs.append(request)
+            set_score_only_batch(request)
             request_id = request.request_id
             req_to_new_blocks[request_id] = new_blocks
             num_scheduled_tokens[request_id] = num_new_tokens
@@ -593,6 +609,11 @@ class Scheduler(SchedulerInterface):
                     )
                 ):
                     # Scheduling would exceed max_loras, skip.
+                    self.waiting.pop_request()
+                    skipped_waiting_requests.prepend_request(request)
+                    continue
+
+                if not matches_score_only_batch(request):
                     self.waiting.pop_request()
                     skipped_waiting_requests.prepend_request(request)
                     continue
@@ -785,6 +806,7 @@ class Scheduler(SchedulerInterface):
                 else:
                     raise RuntimeError(f"Invalid request status: {request.status}")
 
+                set_score_only_batch(request)
                 if self.lora_config and request.lora_request:
                     scheduled_loras.add(request.lora_request.lora_int_id)
                 req_to_new_blocks[request_id] = self.kv_cache_manager.get_blocks(
@@ -1360,6 +1382,16 @@ class Scheduler(SchedulerInterface):
                 # Pooling stops as soon as there is output.
                 request.status = RequestStatus.FINISHED_STOPPED
                 stopped = True
+
+            if request.score_only:
+                assert not new_token_ids, (
+                    "score_only requests must not generate new tokens"
+                )
+                if request.num_computed_tokens >= (
+                    request.num_tokens + request.num_output_placeholders
+                ):
+                    request.status = RequestStatus.FINISHED_LENGTH_CAPPED
+                    stopped = True
 
             routed_experts = None
             finish_reason = None

TomerBN-Nvidia/vllm

vllm/v1/core/sched/scheduler.py
@@ -334,16 +334,29 @@ class Scheduler(SchedulerInterface):
         encoder_compute_budget = self.max_num_encoder_input_tokens
         # Spec decode-related.
         scheduled_spec_decode_tokens: dict[str, list[int]] = {}
+        score_only_batch: bool | None = None
 
         # For logging.
         scheduled_timestamp = time.monotonic()
 
         self.kv_cache_manager.new_step_starts()
 
+        def matches_score_only_batch(request: Request) -> bool:
+            return score_only_batch is None or request.score_only == score_only_batch
+
+        def set_score_only_batch(request: Request) -> None:
+            nonlocal score_only_batch
+            if score_only_batch is None:
+                score_only_batch = request.score_only
+
         # First, schedule the RUNNING requests.
         req_index = 0
         while req_index < len(self.running) and token_budget > 0:
             request = self.running[req_index]
+
+            if not matches_score_only_batch(request):
+                req_index += 1
+                continue
 
             if (
                 request.num_output_placeholders > 0
@@ -447,6 +460,8 @@ class Scheduler(SchedulerInterface):
                             preempted_encoder_inputs = scheduled_encoder_inputs.pop(
                                 preempted_req_id, None
                             )
+                            if not num_scheduled_tokens:
+                                score_only_batch = None
                             if preempted_encoder_inputs:
                                 # Restore encoder compute budget if the preempted
                                 # request had encoder inputs scheduled in this step.
@@ -471,6 +486,7 @@ class Scheduler(SchedulerInterface):
 
             # Schedule the request.
             scheduled_running_reqs.append(request)
+            set_score_only_batch(request)
             request_id = request.request_id
             req_to_new_blocks[request_id] = new_blocks
             num_scheduled_tokens[request_id] = num_new_tokens
@@ -558,6 +574,11 @@ class Scheduler(SchedulerInterface):
                     )
                 ):
                     # Scheduling would exceed max_loras, skip.
+                    request_queue.pop_request()
+                    step_skipped_waiting.prepend_request(request)
+                    continue
+
+                if not matches_score_only_batch(request):
                     request_queue.pop_request()
                     step_skipped_waiting.prepend_request(request)
                     continue
@@ -772,6 +793,7 @@ class Scheduler(SchedulerInterface):
                 else:
                     raise RuntimeError(f"Invalid request status: {request.status}")
 
+                set_score_only_batch(request)
                 if self.lora_config and request.lora_request:
                     scheduled_loras.add(request.lora_request.lora_int_id)
                 req_to_new_blocks[request_id] = self.kv_cache_manager.get_blocks(
@@ -1353,6 +1375,16 @@ class Scheduler(SchedulerInterface):
                 # Pooling stops as soon as there is output.
                 request.status = RequestStatus.FINISHED_STOPPED
                 stopped = True
+
+            if request.score_only:
+                assert not new_token_ids, (
+                    "score_only requests must not generate new tokens"
+                )
+                if request.num_computed_tokens >= (
+                    request.num_tokens + request.num_output_placeholders
+                ):
+                    request.status = RequestStatus.FINISHED_LENGTH_CAPPED
+                    stopped = True
 
             if new_token_ids and self.structured_output_manager.should_advance(request):
                 struct_output_request = request.structured_output_request